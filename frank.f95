589541d9686505963f5c6ea6beb51f5dfdcc6ef0
Avi Greenbury <avi@moses.(none)>
2010-02-24 00:29:16 +0000

    Whoo. fortran

---
+++ frank.f95
@@ -0,0 +1,30 @@
+program FRANK
+real p, t
+real maths
+integer ps(10), ts(10)
+
+ps=(/1, 2, 3, 5, 8, 13, 21, 24, 45, 69 /)
+ts(1)=44
+
+! sub to do the maths.
+! T1 = a
+! P1 = b
+! P2 = c
+! (T2 = d)
+subroutine maths(a,b,c,d)
+	real a,b,c,d
+	d=a(c/b)
+	return
+end
+
+! Iterate through the ps array, and calculate the Ts
+do 10 count = 0, 10, 1
+	call maths(ts(i), p(i), p(i-1))
+	ts(i+1)=d
+10 continue
+
+do 20 count = 0, 10, 1
+	print *, ps(i)
+20 continue
+
+end program
