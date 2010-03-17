a559c03f97e20cf88734703f6878958063a46527
Avi Greenbury <avi@jup-linux2.SGL.GROUP>
2010-03-17 09:01:42 +0000

    Added a whole bunch of crap code

--- alex/contact.php
+++ alex/contact.php
@@ -1,82 +1,205 @@
 <?php
-if(isset($_POST['email'])) {
-	
+
+$me = $_SERVER['PHP_SELF']; 
+
+if($_POST['submit']!='Submit'){
+	echo ("
+		<form name='contactform' method='post' action='$me'>
+		 	<table width='450px'>
+				</tr>
+				<tr>
+				 <td valign='top'>
+				  <label for='first_name'>First Name *</label>
+				 </td>
+				 <td valign='top'>
+				  <input  type='text' name='first_name' maxlength='50' size='30'>
+				 </td>
+				</tr>
+				<tr>	
+				 <td valign='top'>
+				  <label for='last_name'>Last Name *</label>
+				 </td>
+				 <td valign='top'>
+				  <input  type='text' name='last_name' maxlength='50' size='30'>
+				 </td>
+				</tr>
+				<tr>	
+				 <td valign='top'>
+				  <label for='email'>Email Address *</label>
+				 </td>
+				 <td valign='top'>
+				  <input  type='text' name='email' maxlength='80' size='30'>
+				 </td>
+				</tr>
+				<tr>
+				 <td valign='top'>
+				  <label for='telephone'>Telephone Number</label>
+				 </td>	
+		 		 <td valign='top'>
+				  <input  type='text' name='telephone' maxlength='30' size='30'>
+				 </td>	
+				</tr>
+				<tr>
+				 <td valign='top'>
+				  <label for='comments'>Comments *</label>
+				 </td>
+				 <td valign='top'>
+				  <textarea  name='comments' maxlength='1000' cols='25' rows='6'></textarea>
+				 </td>	
+				</tr>
+				<tr>
+				 <td colspan='2' style='text-align:center'>
+				  <input type='submit' value='Submit' name='submit'>  <!--<a href='http://www.freecontactform.com/email_form.php'>Email Form</a>-->
+				 </td>
+				</tr>
+			</table>
+		</form>
+	");
+}else{
 	// EDIT THE 2 LINES BELOW AS REQUIRED
 	$email_to = "you@yourdomain.com";
 	$email_subject = "Your email subject line";
-	
-	
-	function died($error) {
-		// your error code can go here
-		echo "We are very sorry, but there were error(s) found with the form your submitted. ";
-		echo "These errors appear below.<br /><br />";
-		echo $error."<br /><br />";
-		echo "Please go back and fix these errors.<br /><br />";
-		die();
-	}
-	
-	// validation expected data exists
-	if(!isset($_POST['first_name']) ||
-		!isset($_POST['last_name']) ||
-		!isset($_POST['email']) ||
-		!isset($_POST['telephone']) ||
-		!isset($_POST['comments'])) {
-		died('We are sorry, but there appears to be a problem with the form your submitted.');		
-	}
-	
-	$first_name = $_POST['first_name']; // required
-	$last_name = $_POST['last_name']; // required
-	$email_from = $_POST['email']; // required
-	$telephone = $_POST['telephone']; // not required
-	$comments = $_POST['comments']; // required
-	
-	$error_message = "";
-	$email_exp = "^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$";
-  if(!eregi($email_exp,$email_from)) {
-  	$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-	$string_exp = "^[a-z .'-]+$";
-  if(!eregi($string_exp,$first_name)) {
-  	$error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!eregi($string_exp,$last_name)) {
-  	$error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
-  if(strlen($comments) < 2) {
-  	$error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
-  $string_exp = "^[0-9 .-]+$";
-  if(!eregi($string_exp,$telephone)) {
-  	$error_message .= 'The Telphone Number you entered does not appear to be valid.<br />';
-  }
-  if(strlen($error_message) > 0) {
-  	died($error_message);
-  }
-	$email_message = "Form details below.\n\n";
-	
-	function clean_string($string) {
-	  $bad = array("content-type","bcc:","to:","cc:","href");
-	  return str_replace($bad,"",$string);
-	}
-	
-	$email_message .= "First Name: ".clean_string($first_name)."\n";
-	$email_message .= "Last Name: ".clean_string($last_name)."\n";
-	$email_message .= "Email: ".clean_string($email_from)."\n";
-	$email_message .= "Telephone: ".clean_string($telephone)."\n";
-	$email_message .= "Comments: ".clean_string($comments)."\n";
-	
-	
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
 
-<!-- include your own success html here -->
+	// Some laziness. I don't like long variable names. Also provides an easy 
+	// way to see just how much of their code I plagiarised:
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$telephone = $_POST['telephone'];
+	$comments = $_POST['comments'];
 
-Thank you for contacting us. We will be in touch with you very soon.
+	// This chunk is checking that each submission is non-zero. This is (currently) 
+	// all the validation we bother to do.
+	// If any are non-zero, it re-presents the form for filling in again. 
+	// This gets called again on next form submission, so with a really stupid user
+	// the process can go on forever. This is considered a feature.
 
-<?
+	// Everything that is indented as far as the line below the one below this 
+	// chunk (beginning 'empty ($_POST['last_name']' ) up to the next thing that 
+	// lines up with here (i.e. one tab in, or in line with if(empty)) is executed 
+	// when the form is submitted incorrectly filled in. 
+
+	if(empty($_POST['first_name']) ||
+		empty($_POST['last_name']) ||
+		empty($_POST['email']) ||
+//		empty($_POST['telephone']) ||
+		empty($_POST['comments'])) { 
+
+		echo ("	
+			<form name='contactform' method='post' action='$me'>
+			 	<table width='450px'>
+					</tr>
+					<tr>
+					 <td valign='top'>
+		");
+		if (empty($first_name)){
+			echo ("	
+					<label for='first_name'><strong>First Name *</strong></label>
+			");
+		}else{
+			echo ("
+					<label for='first_name'>First Name *</label>
+			");
+		}
+		echo("
+					 </td>
+					 <td valign='top'>
+					  <input  type='text' name='first_name' maxlength='50' size='30' value='$first_name'>
+					 </td>
+					</tr>
+					<tr>	
+					 <td valign='top'>
+		");
+		if (empty($last_name)){
+			echo("
+					<label for='last_name'><strong>Last Name *</strong></label>
+			");
+		}else{
+			echo("
+					  <label for='last_name'>Last Name *</label>
+			");
+		}
+		echo ("
+					 </td>
+					 <td valign='top'>
+					  <input  type='text' name='last_name' maxlength='50' size='30' value='$last_name'>
+					 </td>
+					</tr>
+					<tr>	
+					 <td valign='top'>
+		");
+		if (empty($email)){
+			echo("
+					  <label for='email'><strong>Email Address *</strong></label>
+			");
+		}else{
+			echo("
+					  <label for='email'>Email Address *</label>
+			");
+		}
+		echo("
+					 </td>
+					 <td valign='top'>
+					  <input  type='text' name='email' maxlength='80' size='30' value='$email'>
+					 </td>
+					</tr>
+					<tr>
+					 <td valign='top'>
+					  <label for='telephone'>Telephone Number</label>
+					 </td>	
+			 		 <td valign='top'>
+					  <input  type='text' name='telephone' maxlength='30' size='30'>
+					 </td>	
+					</tr>
+					<tr>
+					 <td valign='top'>
+		");
+		if (empty($comments)){
+			echo ("
+					  <label for='comments'><strong>Comments *</strong></label>
+			");
+		}else{			  
+			echo("
+					  <label for='comments'>Comments *</label>
+			");
+		}
+		echo("
+					 </td>
+					 <td valign='top'>
+					  <textarea  name='comments' maxlength='1000' cols='25' rows='6'>$comments</textarea>
+					 </td>	
+					</tr>
+					<tr>
+					 <td colspan='2' style='text-align:center'>
+					  <input type='submit' value='Submit' name='submit'>  <!--<a href='http://www.freecontactform.com/email_form.php'>Email Form</a>-->
+					 </td>
+					</tr>
+				</table>
+			</form>
+		
+		");	
+	
+				
+	}else{
+	// This bunch of code is only executed if the form is filled in satisfactorally
+	// it's worth bearing in mind that 'satisfactorally' means 'has some data in it'.
+
+	
+		// create email headers
+		$headers = 'From: '.$email_from."\r\n".
+		'Reply-To: '.$email_from."\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+		// @mail($email_to, $email_subject, $comments, $headers);  
+		echo" (@mail($email_to,$email_subject, $comments, $headers) ";
+
+		echo ("
+	
+		<!-- include your own success html here -->
+
+
+
+		");
+	}
 }
+?>
 
