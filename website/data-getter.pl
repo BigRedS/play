af1325efa6a49a69694cff9a588fd942747cd797
Avi <ialoneambest@gmail.com>
2010-04-01 16:15:27 +0100

    Split index.pl up into data-getter and index

---
+++ website/data-getter.pl
@@ -0,0 +1,82 @@
+#! /usr/bin/perl
+
+use strict;
+use 5.010;
+
+use XML::RAI;
+use XML::RSSLite;
+use XML::FeedPP;
+use LWP::Simple;
+use DateTime;
+use DateTime::Format::Atom;
+
+my $file = "./data";
+
+my %stuff = (&slashdot(), &atom());
+
+
+open (FILE , ">$file") || die ("Error opening $file");
+for my $key (reverse sort (%stuff)){
+	if ($stuff{$key}){			# This shouldn't be necessary.
+		my $date = $key;
+		my $arrayref = %stuff->{$key};
+		my @array = @$arrayref;
+		
+		print FILE "$date \n@array[0] \n@array[1]\n\n";
+	}
+}
+close FILE;
+
+sub atom(){
+
+	my @urls = ("http://identi.ca/api/statuses/user_timeline/99267.atom", "http://forums.theregister.co.uk/feed/user/40790", "http://github.com/BigRedS.atom");
+	my %return;
+
+	foreach my $url (@urls){
+		my $feed = XML::FeedPP->new( $url );
+
+		foreach my $item ( $feed->get_item() ) {
+			my $d = DateTime::Format::Atom->new();
+			my $dt = $d->parse_datetime( $item->pubDate );
+			my $time = $dt->epoch;
+	
+			$return{ $time } = [$item->link, $item->title()];
+		}
+	}
+	return %return;
+}
+
+sub slashdot(){
+	my %return;
+	my $url = "http://slashdot.org/firehose.pl?op=rss&content_type=rss&view=userhomepage&fhfilter=%22home%3Alordandmaker%22&orderdir=DESC&orderby=createtime&color=black&duration=-1&startdate=&user_view_uid=960504&logtoken=960504%3A%3AqQuPZQpQoZTAkoJGfmbG6g";
+	my $content = get($url);
+	my $rai = XML::RAI->parse_string($content);
+	foreach my $item ( @{$rai->items} ) {
+		$content = substr($item->content, 0, 100);
+		$content.="...";  
+
+
+		## This was supposed to be done by TimeDate::Format::RSS
+		## but I couldn't persuade CPAN to tell me why it	
+		## couldn't install it.				
+
+		my $time = $item->issued;			
+		my ($date, $time) = split (/T/, $time);		
+		my ($time, $tz) = split (/\+/, $time);		
+		my ($year,$month,$day) = split(/-/, $date);	
+		my ($h,$m,$s) = split(/:/, $time);
+		my $dt = DateTime->new( 
+			year   => $year,
+			month  => $month,
+			day    => $day,
+			hour   => $h,
+			minute => $m,
+			second => $s,
+			time_zone => "+$tz",
+                 );
+		$time = $dt->epoch;
+		my $link = $item->link;
+		$return{$time} = [$link, $content]
+	}
+	return %return
+}
--- website/index.pl
+++ website/index.pl
@@ -3,122 +3,31 @@
 use strict;
 use 5.010;
 
-use XML::RAI;
-use XML::RSSLite;
-use XML::FeedPP;
-use LWP::Simple;
-use DateTime;
-use DateTime::Format::Atom;
+print "Content-type: text/html\n\n\n";
 
-#print "Content-type: text/html\n\n\n";
+my $data = "./data";
 
+open(DATA, "<$data")
+ or print "Error opening data file";
 
-## Get some data!
-#my %stuff = (&el_reg(), &slashdot(), &identi() );
-
-my %stuff = (&slashdot(), &atom());
-
-## Let's make some HTML!
 &start_html();
-
-for my $key (reverse sort (%stuff)){
-
-	if ($stuff{$key}){			# This shouldn't be necessary.
-		my $date = $key;		# Fix it.
-		my @array = %stuff->{$key};
-						# Fix it now.
-		&make_tr($date, @array);
+{
+	local $/ = "";
+	while (<DATA>){
+		my ($date, $link, $content) = (split(/\n/, $_))[0,1,2];
+		&make_tr($date, $link, $content);
 	}
 }
-
-## Let's stop.
 &end_html();
 
+close DATA;
 
-
-#	Here Be Subroutines					#
-#								#
-# Subs to grab content and stick it in a hash. Hashes are of	#
-# the form 							#
-#	$return{date} = ["url", "text"]				#
-# where date is a unix timestamp				#
-
-
-
-sub atom(){
-
-	#does both The Register and Identi.ca, since they appear to use standard atom feeds.
-
-	my @urls = ("http://identi.ca/api/statuses/user_timeline/99267.atom", "http://forums.theregister.co.uk/feed/user/40790", "http://github.com/BigRedS.atom");
-	my %return;
-
-	foreach my $url (@urls){
-		my $feed = XML::FeedPP->new( $url );
-
-		foreach my $item ( $feed->get_item() ) {
-			my $d = DateTime::Format::Atom->new();
-			my $dt = $d->parse_datetime( $item->pubDate );
-			my $time = $dt->epoch;
-	
-			$return{ $time } = [$item->link, $item->title()];
-		}
-	}
-	return %return;
-}
-
-sub slashdot(){
-	my %return;
-	my $url = "http://slashdot.org/firehose.pl?op=rss&content_type=rss&view=userhomepage&fhfilter=%22home%3Alordandmaker%22&orderdir=DESC&orderby=createtime&color=black&duration=-1&startdate=&user_view_uid=960504&logtoken=960504%3A%3AqQuPZQpQoZTAkoJGfmbG6g";
-	my $content = get($url);
-	my $rai = XML::RAI->parse_string($content);
-	foreach my $item ( @{$rai->items} ) {
-		$content = substr($item->content, 0, 100);
-		$content.="...";  
-
-
-		## This was supposed to be done by TimeDate::Format::RSS
-		## but I couldn't persuade CPAN to tell me why it	
-		## couldn't install it.				
-
-		my $time = $item->issued;			
-		my ($date, $time) = split (/T/, $time);		
-		my ($time, $tz) = split (/\+/, $time);		
-		my ($year,$month,$day) = split(/-/, $date);	
-		my ($h,$m,$s) = split(/:/, $time);
-		my $dt = DateTime->new( 
-			year   => $year,
-			month  => $month,
-			day    => $day,
-			hour   => $h,
-			minute => $m,
-			second => $s,
-			time_zone => "+$tz",
-                 );
-		$time = $dt->epoch;
-		my $link = $item->link;
-		$return{$time} = [$link, $content]
-	}
-	return %return
-}
-
-sub identi(){
-	
-
-
-
-}
-
-# Boring subs 							#
-
-								
-# Accepts as an argument the two items spouted by the above 	#
-# get-data-subs (a string which is the timestamp and a 		#
-# two-element array containing a URL and some text to use as 	#
-# the link.							#
+# # # 
 
 sub make_tr(){
-	my ($date, $arrayref) = @_;
-	my ($link, $content) = @$arrayref[0,1];
+	my $date = shift;
+	my $link = shift;
+	my $content = shift;
 
 	$date = &friendly_date($date);
 
@@ -126,11 +35,6 @@ sub make_tr(){
 	print get_icon($link);
 	say "</td><td class='text'><a href='$link'>$content</a></td><td class='date'>$date</td></tr>";
 }
-
-
-
-## Decides which icon to use based on (presumably) a URL passed to it, and 
-## returns the HTML to display that icon (i.e. <img src=".....">
 
 sub get_icon{
 	my ($url, $alt, $link);
