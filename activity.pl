74e09de3d5c8ffec5bd4e42a0234808b5940f76e
Avi <ialoneambest@gmail.com>
2010-07-26 00:21:47 +0100

    added comments`

--- activity.pl
+++ activity.pl
@@ -1,11 +1,23 @@
 #! /usr/bin/perl
 
+# Iterates through a list of hostnames, and inspects syslog for
+# mention of them, then says who's had attention and makes an 
+# educated guess as to when it last had activity.
+
+# Doesn't yet know when to stop with log rotations.
+
 use strict;
 use 5.010;
 
+# Looks for files matching syslog* in here, greps them for
+# evidence of activity.
 my $logDir = "./logs";
+
+# File with a list of domains. Like qmail's rcpthosts file.
 my $hostList = "./hosts.txt";
 
+# Create array of hostnames consisting of non-empty lines
+# from $hostList
 my %hosts;
 open (my $hl, "<$hostList");
 	foreach my $line (<$hl>){
@@ -16,11 +28,11 @@ open (my $hl, "<$hostList");
 	}
 close $hl;
 
-
 my @logs = `ls -1 $logDir/syslog*`;
-my @matches;
 my @log;
 
+# Get all the lines containing '@' out of the logfiles, and 
+# concatenate them into one easy-to-use array
 foreach my $logFile (@logs){
 	if ($logFile =~ /\.gz$/){
 		foreach (`zcat $logFile`){
@@ -39,12 +51,16 @@ foreach my $logFile (@logs){
 	}
 }
 
+# Apparently vain attempt to ensure that the date recorded in the
+# hash is the latest one from the logs.
 @log = reverse @log;
 
-foreach my $host (keys %hosts){
 
+# Iterate through the hosts list, for each one see if we can find 
+# mention in @log. If found, sticks the date it found in the hash. 
+# Hopefully, that date is the last date we saw the domain.
+foreach my $host (keys %hosts){
 	chomp $host;
-	LOG:
 	foreach my $line (@log){
 		if ($line =~ /$host/i){
 			my $date = join(" ", (split(/\s/, $line))[0,1,2]);
@@ -53,6 +69,9 @@ foreach my $host (keys %hosts){
 		}
 	}
 }
+
+
+# This is dumb and should be smarter:
 
 say "Hosts with activity:";
 foreach my $host (keys %hosts){
@@ -67,6 +86,5 @@ foreach my $host (keys %hosts){
 		say "\t$host";
 	}
 }
-
 
 exit 0;
