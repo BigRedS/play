e7bed8b68378ba5bc912d4293dec78e002aa7ce0
Avi <ialoneambest@gmail.com>
2010-09-29 23:42:26 +0100

    Now accepts file-list of addresses as its first argument.

--- sqwebmail-preconfigure.pl
+++ sqwebmail-preconfigure.pl
@@ -1,26 +1,42 @@
 #! /usr/bin/perl
 
-if (!$ARGV[1]){
+# Preconfigures squirrelmail for addresses. Either call it
+# sqwebmail-preconfigure "full name" email@address.com
+# or with the path to a file as the first argument, that file
+# containing a list of the form
+# full name, email@address.com
+
+if ( (!$ARGV[1]) && (! (-f $ARGV[0])) ){
 	print "Usage:\n\t$0 \"[FULL-NAME]\" [EMAIL-ADDRESS]\n";
 	exit 1;
 }
 
-my $name = $ARGV[0];
-my $email = $ARGV[1];
-my $filename;
-
-if ($email =~ /(.+\@[^\.]+)\.+/){
-	 $filename = $1;
+if ( -f $ARGV[0] && !$ARGV[1] ){
+	open ($fh, "<", $ARGV[0]);
+	while(<$fh>){
+		my($name, $email) = split(/,/);
+		&preconfigure($name, $email);
+	}
 }else{
-	print "Couldn't deduce filename from email address\n";
-	print "You'll have to do this one manually, or fix the script";
-	exit 1;
+	&preconfigure($ARGV[0], $ARGV[1]);
 }
 
-$filename =~ s/\@/\./;
-$filename .= ".pref";
+sub preconfigure(){
+	my $name = shift;
+	my $email = shift;
+	my $filename;
 
-`echo -e \"full_name=$name\" >> /var/lib/squirrelmail/data/$filename`;
-`echo -e \"email_address=$email\" >> /var/lib/squirrelmail/data/$filename`;
-`chown www-data:www-data /var/lib/squirrelmail/data/$filename`;
+	if ($email =~ /(.+\@[^\.]+)\.+/){
+		 $filename = $1;
+	}else{
+		print "Couldn't deduce filename from email address ($email); you'll have to do this one manually, or fix the script\n";
+		exit 1;
+	}
 
+	$filename =~ s/\@/\./;
+	$filename .= ".pref";
+
+	print "`echo -e \"full_name=$name\" >> /var/lib/squirrelmail/data/$filename`;\n ";
+	print "`echo -e \"email_address=$email\" >> /var/lib/squirrelmail/data/$filename`;\n ";
+	print "`chown www-data:www-data /var/lib/squirrelmail/data/$filename`; \n";
+}
