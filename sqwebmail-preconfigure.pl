a310339e3c26dbcea47b18dfa3d083f8a67785f2
BigRedS <ialoneambest@gmail.com>
2010-10-01 10:05:04 +0100

    now expects files in the same format as we're sent them

--- sqwebmail-preconfigure.pl
+++ sqwebmail-preconfigure.pl
@@ -2,15 +2,37 @@
 
 # Preconfigures squirrelmail for new email addresses; makes it use
 # the correct From: field data.
-# 
-# Accepts input in three directions:
-#   as an argument of the form  "full name" email@address.com
-#   in a file given as the first argument
-#   piped through on stdin
+
+# Accepts input from three directions:
+# Specified as an argument, in the form:
+# sqwebmail-preconfigure fullname email
+# If 'fullname' contains a space it must be in quote makrs.
+
+# You can pass a filename as the first argument, or pipe in some data,
+# each must be in the same format, which is how they appear to record it,
+# or at least the layout that they feel makes the most sense for sending. 
+# Here's a sample, obviously the real file shouldn't have hashes at the 
+# beginning of each line:
+# (the blank lines are not mandatory, and neither is the capitalisation).
+
+#1. Domain: www.londonbtxcentre.co.uk
 #
-# where the file and stdin are linebreak-separated lists with each
-# element of the form
-# full name, email@address.com
+#Email: info@
+#Full Name: Info Londonbtxcentre
+#
+#Email: dr.mhmarion@
+#Full Name: Dr. Marie-Helene Marion
+#
+#2. Domain: www.anglehouseorthodontics.co.uk
+#
+#Email: bhavnita@
+#Full Name: Bhavnita
+#
+#Email: carlene@
+#Full Name: Carlene
+
+# The latest version of this is at 
+# http://github.com/BigRedS/play/raw/master/sqwebmail-preconfigure.pl
 
 
 if ( (!$ARGV[1]) && (! (-f $ARGV[0])) && (-t STDIN) ) {
@@ -27,14 +49,25 @@ if (( -f $ARGV[0] && !$ARGV[1]) || ( !-t STDIN )) {
 	}else{
 		open ($fh, "<", $ARGV[0]);
 	}
+	my ($domain, $lhs, $name);
 	while(<$fh>){
-		my($name, $email) = split(/,/);
-		chomp $email;
-		&preconfigure($name, $email);
+		if (/Domain: (.+)+\n/i){
+			$domain = $1;
+			$domain =~ s/^www\.//;
+		}
+		if (/^Email:\s(\w+)\@?\n/i){
+			$lhs = $1;
+		}
+		if (/^Full Name: (.+)\n/i){
+			$name = $1;
+			$email = $lhs."@".$domain;
+			&preconfigure($name, $email);
+		}
 	}
 }else{
 	&preconfigure($ARGV[0], $ARGV[1]);
 }
+
 
 sub preconfigure(){
 	my $name = shift;
@@ -53,7 +86,7 @@ sub preconfigure(){
 	$filename =~ s/\@/\./;
 	$filename .= ".pref";
 
-	print "`echo -e \"full_name=$name\" >> /var/lib/squirrelmail/data/$filename`;\n ";
-	print "`echo -e \"email_address=$email\" >> /var/lib/squirrelmail/data/$filename`;\n ";
-	print "`chown www-data:www-data /var/lib/squirrelmail/data/$filename`; \n";
+	`echo -e \"full_name=$name\" >> /var/lib/squirrelmail/data/$filename`;
+	`echo -e \"email_address=$email\" >> /var/lib/squirrelmail/data/$filename`;
+	`chown www-data:www-data /var/lib/squirrelmail/data/$filename`;
 }
