feca32ada1aac53bb5c88480958fff9de0cbddae
Avi Greenbury <ialoneambest@gmail.com>
2011-05-26 18:28:51 +0100

    Oh yeah, security

--- ukmb.pl
+++ ukmb.pl
@@ -9,25 +9,29 @@ print "content-type: text/html\n\n";
 
 if(defined(param('url'))){
 
-	my $username = "";
-	my $password = "";
+	my $username = "Automaton";
+	my $password = ""
 	my $page = param('url');
+        if ($page =~ /^http:\/\/ukmb.net\/forum\//){
+		my $mech = WWW::Mechanize->new();
 
-	my $mech = WWW::Mechanize->new();
+		$mech->get($page);
+		$mech->form_number(1);
+		$mech->field("user" => $username);
+		$mech->field("passwrd" => $password);
+		$mech->field("cookielength" => "60");
+		my $request = $mech->click();
 
-	$mech->get($page);
-	$mech->form_number(1);
-	$mech->field("user" => $username);
-	$mech->field("passwrd" => $password);
-	$mech->field("cookielength" => "60");
-	my $request = $mech->click();
-
-	my ($input, $output);
-	$input = $request->content;
+		my ($input, $output);
+		$input = $request->content;
 	
-	gunzip (\$input => \$output);
+		gunzip (\$input => \$output);
 	
-	print $output;
+		print $output;
+	}else{
+		print "That looks like an invalid URL <br />";
+		print "URLs must begin \"http://ukmb.net/forum/\""
+	}
 }else{
 	print <<EOF;
 <html>
